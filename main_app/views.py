51f68f1a989780951e1579ed87b4cb43ce3d574e
Dustin Crawford <dustin.crawford.2@gmail.com>
2020-07-10 16:24:40 -0400

    completed form and result page

--- main_app/views.py
+++ main_app/views.py
@@ -2,4 +2,18 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello World')
+    return render(request, 'index.html')
+
+def result(request):
+    fullName = request.POST['fullName']
+    location = request.POST['location']
+    language = request.POST['language']
+    comment = request.POST['comment']
+
+    context = {
+        'name': fullName,
+        'loc': location,
+        'lang': language,
+        'comm': comment,
+    }
+    return render(request, 'result.html', context)
